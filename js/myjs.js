75ab4d691e772d95418ce5ac626b65414eda96d6
vvin55318 <vvin5318@gmail.com>
2020-02-08 16:57:53 +0800

    update

---
+++ js/myjs.js
@@ -0,0 +1,57 @@
+// date
+var tyear=new Date().getFullYear()
+$("#copyright")[0].textContent=`© ${tyear} WenYen`
+  
+// JS_StoryGame滑入 彈出
+$("#fa_img_StoryGame").mouseover(function(){
+    $("#fa_txt_StoryGame").css({"display":"block"})
+})
+
+$("#fa_img_StoryGame").mouseout(function(){
+    $("#fa_txt_StoryGame").css({"display":"none"})
+})
+
+// JS_ColorPicker滑入 彈出
+$("#fa_img_ColorPicker").mouseover(function(){
+    $("#fa_txt_ColorPicker").css({"display":"block"})
+})
+
+$("#fa_img_ColorPicker").mouseout(function(){
+    $("#fa_txt_ColorPicker").css({"display":"none"})
+})
+
+// JS_AnalogClock滑入 彈出
+$("#fa_img_AnalogClock").mouseover(function(){
+    $("#fa_txt_AnalogClock").css({"display":"block"})
+})
+
+$("#fa_img_AnalogClock").mouseout(function(){
+    $("#fa_txt_AnalogClock").css({"display":"none"})
+})
+
+// JS_DigitalClock滑入 彈出
+$("#fa_img_DigitalClock").mouseover(function(){
+    $("#fa_txt_DigitalClock").css({"display":"block"})
+})
+
+$("#fa_img_DigitalClock").mouseout(function(){
+    $("#fa_txt_DigitalClock").css({"display":"none"})
+})
+
+// JS_OpenAPI滑入 彈出
+$("#fa_img_OpenAPI").mouseover(function(){
+    $("#fa_txt_OpenAPI").css({"display":"block"})
+})
+
+$("#fa_img_OpenAPI").mouseout(function(){
+    $("#fa_txt_OpenAPI").css({"display":"none"})
+})
+
+// San'sLogic滑入 彈出
+$("#fa_img_SansLogic").mouseover(function(){
+    $("#fa_txt_SansLogic").css({"display":"block"})
+})
+
+$("#fa_img_SansLogic").mouseout(function(){
+    $("#fa_txt_SansLogic").css({"display":"none"})
+})
